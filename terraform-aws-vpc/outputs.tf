0bcadc1c1440c181fd54b5b60fea5011cfd73f2f
sainath <kollavenkatasai999@gmail.com>
2024-06-19 09:45:43 -0500

    commit

--- terraform-aws-vpc/outputs.tf
+++ terraform-aws-vpc/outputs.tf
@@ -1,3 +1,19 @@
 # output "azs" {
 #     value = data.aws_availability_zones.available.names # because we require available zone names
 # }
+
+output "vpc_id" {
+    value = aws_vpc.main.id
+}
+
+output "public_subnet" {
+    value = aws_subnet.public[*].id
+}
+
+output "private_subnet" {
+    value = aws_subnet.private[*].id
+}
+
+output "database_subnet" {
+    value = aws_subnet.database[*].id
+}
