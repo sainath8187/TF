33ac141a1edd229cf712c8d66bf34fd52254f71c
sainath <kollavenkatasai999@gmail.com>
2024-06-19 09:20:13 -0500

    commit

--- terraform-aws-vpc-test/vpc.tf
+++ terraform-aws-vpc-test/vpc.tf
@@ -4,4 +4,7 @@ module "vpc" {
     common_tags = var.common_tags
     environment = var.environment
     public_subnet_cidrs = var.public_subnet_cidrs
-}
+    private_subnet_cidrs = var.private_subnet_cidrs
+    database_subnet_cidrs = var.database_subnet_cidrs
+    is_peering_required = var.is_peering_required
+}    
